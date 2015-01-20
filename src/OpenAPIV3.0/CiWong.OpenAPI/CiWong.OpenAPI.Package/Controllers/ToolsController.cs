60395730edc4f61c5bb50552e7db1c06ea171170
钟永华 <zhongyonghua@ciwong.com>
2015-01-20 18:11:16 +0800

    2015-01-15 18:11

--- src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.Package/Controllers/ToolsController.cs
+++ src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.Package/Controllers/ToolsController.cs
@@ -5,40 +5,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using CiWong.OpenAPI.Core;
 using CiWong.Tools.Workshop.DataContracts;
 using CiWong.Tools.Workshop.Services;
 
 namespace CiWong.OpenAPI.ToolsAndPackage.Controllers
 {
-	public class ToolsController: ApiController
-	{
-	    /// <summary>
+    public class ToolsController : ApiController
+    {
+        /// <summary>
         /// 获取课后单词表资源
         /// 说明:选择单词.只有资源类型ID=a7527f97-14e6-44ef-bf73-3039033f128e时才有单词选择
-	    /// </summary>
+        /// </summary>
         /// <param name="versionId">资源版本ID(此处传父资源parentVersionId),必选</param>
-	    /// <returns></returns>
-	    [HttpGet]
-        public void followread_word_details(long versionId)
-	    {
-	        var moduleId = new Guid("a7527f97-14e6-44ef-bf73-3039033f128e");
-	        var words = ResourceServices.Instance.GetByVersionIds(moduleId, new[] {versionId});
-	        return ;
-	        //return new
-	        //{
-	        //    wId,
-	        //    words,
-	        //    wordFile,
-	        //    wordType,
-	        //    words,
-	        //    symbol,
-	        //    syllable,
-	        //    pretations,
-	        //    sentences,
-	        //    sentFile,
-	        //    wordPic
-	        //};
+        /// <returns></returns>
+        [HttpGet]
+        public object followread_word_details(long versionId)
+        {
+            var result = ResourceServices.Instance.GetByVersionIds(ResourceModuleOptions.SyncFollowRead, versionId);
+            if (result.IsSucceed)
+            {
+                var data = (SyncReadContract) result.Data.FirstOrDefault();
+                if (data == null)
+                {
+                    throw new ApiArgumentException("参数versionId错误，未找到指定资源");
+                }
+                var wordVersionList =
+                    data.Parts.Where(t => t.ModuleId == ResourceModuleOptions.Word)
+                        .Select(t => t.VersionId)
+                        .Where(t => t != null)
+                        .OfType<long>();
 
-	    }
-	}
+                var wordList= ResourceServices.Instance.GetByVersionIds(ResourceModuleOptions.Word, wordVersionList.ToArray());
+                if (result.IsSucceed)
+                {
+                    var words = wordList.Data.Where(t => t != null).OfType<WordContract>();
+                    return words.Select(x => new
+                    {
+                        wId =x.Id,
+                        words = x.Name,
+                        wordFile = x.AudioUrl,
+                        wordType = x.IsExpand,
+                        symbol = x.Symbol,
+                        syllable = x.Syllable,
+                        pretations = x.Pretations,
+                        sentences = x.Sentences,
+                        sentFile = x,
+                        wordPic = x.PictureUrl
+                    });
+                }
+                else
+                {
+                    throw new ApiException(RetEum.ApplicationError, 1, "内部代码异常");
+                }
+            }
+            else
+            {
+                throw new ApiException(RetEum.ApplicationError, 1, "内部代码异常");
+            }
+        }
+    }
 }
