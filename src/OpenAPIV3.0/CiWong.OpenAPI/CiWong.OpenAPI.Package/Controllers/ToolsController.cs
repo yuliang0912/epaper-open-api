d4e35c246ba9db039bd52ab27cc37273a0055595
钟永华 <zhongyonghua@ciwong.com>
2015-01-21 16:17:48 +0800

    20150121 16:17

--- src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.Package/Controllers/ToolsController.cs
+++ src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.Package/Controllers/ToolsController.cs
@@ -48,16 +48,58 @@ namespace CiWong.OpenAPI.ToolsAndPackage.Controllers
 	        return words.Select(x => new
 	        {
 	            wId = x.Id,
-	            words = x.Name,
-	            wordFile = x.AudioUrl,
-	            wordType = x.IsExpand,
-	            symbol = x.Symbol,
-	            syllable = x.Syllable,
-	            pretations = x.Pretations,
+	            words = x.Name ?? "",
+	            wordFile = x.AudioUrl ?? "",
+	            wordType = x.IsExpand == true ? 1 : 0,
+	            symbol = x.Symbol ?? "",
+	            syllable = x.Syllable ?? "",
+	            pretations = x.Pretations ?? "",
 	            sentences = x.Sentences.Any() ? x.Sentences.First().Text : "",
 	            sentFile = x.Sentences.Any() ? x.Sentences.First().AudioUrl : "",
-	            wordPic = x.PictureUrl
+	            wordPic = x.PictureUrl ?? ""
 	        });
 	    }
+
+        /// <summary>
+        /// 获取选择句子
+        /// </summary>
+        /// <param name="versionId">资源版本ID(课文ID),必选</param>
+        /// <returns></returns>
+        [HttpGet]
+        public object followread_text_sentences(long versionId)
+        {
+            var result = ResourceServices.Instance.GetByVersionIds(ResourceModuleOptions.SyncFollowRead, versionId);
+            if (!result.IsSucceed)
+            {
+                throw new ApiException(RetEum.ApplicationError, 2, "内部代码异常");
+            }
+            var data = (SyncFollowReadContract)result.Data.FirstOrDefault();
+            if (data == null)
+            {
+                throw new ApiArgumentException("参数versionId错误，未找到指定资源");
+            }
+            var versionList =
+                data.Parts.Where(t => t.ModuleId == ResourceModuleOptions.SyncFollowReadText)
+                    .SelectMany(t => t.List).Select(t => t.VersionId)
+                    .Where(t => t != null)
+                    .OfType<long>();
+
+            var phraseList = ResourceServices.Instance.GetByVersionIds(ResourceModuleOptions.Phrase,
+                versionList.ToArray());
+
+            if (!phraseList.IsSucceed)
+            {
+                throw new ApiException(RetEum.ApplicationError, 1, "内部代码异常");
+            }
+            var phrases = phraseList.Data.Where(t => t != null).OfType<PhraseContract>();
+            return phrases.Select(x => new
+            {
+                content = x.Content ?? "",
+                audioUrl = x.AudioUrl ?? "",
+                versionId = x.VersionId,
+                resourceModuleId = x.ModuleId,
+                name = x.Name ?? ""
+            });
+        }
 	}
 }
