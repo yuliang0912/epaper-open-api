f85405b8c139c23f8c9128ceb1e5c97c311b059d
余亮 <yuliang@ciwong.com>
2015-02-06 16:35:48 +0800

    新增接口文档以及DLL文件

---
+++ src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.ExpandWork/DTO/FileWorkDTO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiWong.OpenAPI.ExpandWork.DTO
+{
+	/// <summary>
+	/// 作业快传
+	/// </summary>
+	public class FileWorkDTO
+	{
+		/// <summary>
+		/// 做作业ID(作业系统)
+		/// </summary>
+		public long DoWorkId { get; set; }
+
+		/// <summary>
+		/// 附件资源包ID
+		/// </summary>
+		public long RecordId { get; set; }
+
+		/// <summary>
+		/// /作业时长
+		/// </summary>
+		public int WorkLong { get; set; }
+
+		/// <summary>
+		/// 附加留言
+		/// </summary>
+		public string Message { get; set; }
+
+		/// <summary>
+		/// 作业答案
+		/// </summary>
+		public IEnumerable<WorkFileDTO> WorkAnswers { get; set; }
+	}
+}
---
+++ src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.ExpandWork/DTO/GraffitiFileWorkDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CiWong.OpenAPI.ExpandWork.DTO
+{
+	/// <summary>
+	/// 涂鸦附件作业
+	/// </summary>
+	public class GraffitiFileWorkDTO
+	{
+		public GraffitiFileWorkDTO()
+		{
+			this.GraffitiFiles = Enumerable.Empty<GraffitiFileDTO>();
+		}
+
+		public long DoId { get; set; }
+
+		public long DoWorkId { get; set; }
+
+		public IEnumerable<GraffitiFileDTO> GraffitiFiles { get; set; }
+	}
+
+	/// <summary>
+	/// 涂鸦图片信息
+	/// </summary>
+	public class GraffitiFileDTO
+	{
+		public int Sid { get; set; }
+
+		public string Comment { get; set; }
+	}
+}
