f07030cf81695d9d7a8f4d88b6c215faf1e616c7
余亮 <yuliang@ciwong.com>
2015-05-04 15:28:13 +0800

    新增路由控制类

---
+++ src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.ExpandWork/WorkExpandWorkModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using CiWong.Resource.Preview.Service;
+
+namespace CiWong.OpenAPI.ExpandWork
+{
+	public class ResourceModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterModule<CiWong.Resource.Preview.ResourceModule>();
+			base.Load(builder);
+		}
+	}
+}
---
+++ src/OpenAPIV3.0/CiWong.OpenAPI/CiWong.OpenAPI.YiShang/YiShangModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using CiWong.YiShang.YiShang;
+
+namespace CiWong.OpenAPI.YiShang
+{
+	public class YiShangModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<ProductRepository>();
+			base.Load(builder);
+		}
+	}
+}
